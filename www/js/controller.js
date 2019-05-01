8c03e2f93ee95ca1879070db63429096120b6758
Ayyoube Chabih <ayoubmas2@gmail.com>
2019-05-01 15:32:00 +0100

    new design

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,4 +1,4 @@
-function login() {
-    $("#choice").animate( {"margin-left": "-100vw"}, 500, "linear" ); 
-    $("#login").animate( { "opacity": "show"} , 1500 );
+function next(from, to) {
+    $("#" + from).animate( {"margin-left": "-100vw"}, 500, "linear" ); 
+    $("#" + to).animate( { "opacity": "show", "margin-left": "0vw"} , 500 );
 }
