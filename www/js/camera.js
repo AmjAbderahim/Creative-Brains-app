e18990bff20c8ea36d632b30433b06609f1adba2
Ayyoube Chabih <ayoubmas2@gmail.com>
2019-05-10 22:14:41 +0700

    addPost camera test

---
+++ www/js/camera.js
@@ -0,0 +1,15 @@
+function cameraTakePicture() { 
+    navigator.camera.getPicture(onSuccess, onFail, {  
+       quality: 50, 
+       destinationType: Camera.DestinationType.DATA_URL 
+    });  
+    
+    function onSuccess(imageData) { 
+       var image = document.getElementById('img_post_preview'); 
+       image.src = "data:image/jpeg;base64," + imageData; 
+    }  
+    
+    function onFail(message) { 
+       alert('Failed because: ' + message); 
+    } 
+}
